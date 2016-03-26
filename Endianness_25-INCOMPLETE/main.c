1dcf6e3c1ae04631ff533f3c7c563b161ba048e7
elc1798 <subliminalmau5@gmail.com>
2016-03-26 14:09:17 -0400

    :'(

--- Endianness_25-INCOMPLETE/main.c
+++ Endianness_25-INCOMPLETE/main.c
@@ -7,6 +7,9 @@
 #include <assert.h>
 
 char buffer[32];
+char chunk1[32];
+char chunk2[32];
+char chunk4[32];
 
 char get_bit(char c, int i) {
     return (c >> i) & 0x01;
@@ -24,7 +27,6 @@ int main() {
 
         char tmp = 0;
 
-        /* CHUNKS OF 4
         tmp = tmp | (get_bit(buffer[i], 7) << 3);
         tmp = tmp | (get_bit(buffer[i], 6) << 2);
         tmp = tmp | (get_bit(buffer[i], 5) << 1);
@@ -33,9 +35,9 @@ int main() {
         tmp = tmp | (get_bit(buffer[i], 2) << 6);
         tmp = tmp | (get_bit(buffer[i], 1) << 5);
         tmp = tmp | (get_bit(buffer[i], 0) << 4);
-        */
+        chunk4[i] = tmp;
 
-        /* CHUNKS OF 2 */
+        tmp = 0;
         tmp = tmp | (get_bit(buffer[i], 7) << 5);
         tmp = tmp | (get_bit(buffer[i], 6) << 4);
         tmp = tmp | (get_bit(buffer[i], 5) << 7);
@@ -44,9 +46,9 @@ int main() {
         tmp = tmp | (get_bit(buffer[i], 2) << 0);
         tmp = tmp | (get_bit(buffer[i], 1) << 3);
         tmp = tmp | (get_bit(buffer[i], 0) << 2);
-        /* */
+        chunk2[i] = tmp;
 
-        /* CHUNKS OF 1
+        tmp = 0;
         tmp = tmp | (get_bit(buffer[i], 7) << 6);
         tmp = tmp | (get_bit(buffer[i], 6) << 7);
         tmp = tmp | (get_bit(buffer[i], 5) << 4);
@@ -55,11 +57,20 @@ int main() {
         tmp = tmp | (get_bit(buffer[i], 2) << 3);
         tmp = tmp | (get_bit(buffer[i], 1) << 0);
         tmp = tmp | (get_bit(buffer[i], 0) << 1);
-        */
-        buffer[i] = tmp;
+        chunk1[i] = tmp;
+
+        chunk1[i] = chunk1[i] ^ chunk2[i] ^ chunk4[i];
     }
- 
-    printf("%s\n", buffer);
+
+    char wtf[24] = {
+        '\xbe', '\x76', '\x2c', '\x8c', '\x26', '\x76', '\xcc', '\xfa', '\xa6', '\x36',
+        '\x2e', '\x2e', '\x96', '\x36', '\xfa', '\x26', '\x76', '\x2c', '\xfa', '\xe2',
+        '\x92', '\x42', '\xde', '\0'
+    };
+    printf("%s\n\n", chunk1);
+    printf("%s\n\n", chunk2);
+    printf("%s\n\n", chunk4);
+    printf("%s\n\n", wtf);
     close(original_file);
     return 0;
 }
