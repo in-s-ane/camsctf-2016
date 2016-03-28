2dbe0dc442953398f67cb34030887e77f0b6fdb9
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2016-03-28 00:58:00 -0400

    clean up pure evil solution

--- Pure-Evil_100/solve.hs
+++ Pure-Evil_100/solve.hs
@@ -6,12 +6,11 @@ reversal = go 0
   where go a 0 = a
         go a b = let (q,r) = b `quotRem` 10 in go (a*10 + r) q
 -- the above taken from https://stackoverflow.com/questions/26315917/decidein-haskell-if-a-number-is-or-not-a-palindrome-without-using-lists
-
-divisible x = x `mod` 16384 == 0
+-- too lazy to write my own
 
 findSum = sum [x | x<-[1000013824,1000030208..9999999999], palindrome x]
 
 main = do
   putStrLn $ show findSum
 
-{19473563648}
+--{19473563648}
